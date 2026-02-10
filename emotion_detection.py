9e7dfab473bda7dfaec6af9758aa2edee8b127d0
Vyanka09 <vyankateshkulkarni42@gmail.com>
2026-02-10 06:40:26 -0500

    Formatted the code output

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,25 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     body = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url,headers=headers,json=body)
-    return response.text
+    formatted_response = json.loads(response.text)
+    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+    result = {'anger':anger_score,'disgust':disgust_score,'fear':fear_score,
+    'joy':joy_score,'sadness':sadness_score}
+    dominant = ''
+    max_score = 0
+    for emotion, score in result.items():
+        if score>max_score:
+            max_score = score
+            dominant = emotion
+
+    result['dominant_emotion']=dominant
+    return result
